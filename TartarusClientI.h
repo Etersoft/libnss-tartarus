5c4c24c29c29ab55d5fdcafbc834147733ff63b7
Evgeny Sinelnikov <sin@altlinux.ru>
2008-07-18 11:34:26 +0400

    Add missed headers

---
+++ TartarusClientI.h
@@ -0,0 +1,33 @@
+#ifndef __Tartarus_RPC_ClientI_h__
+#define __Tartarus_RPC_ClientI_h__
+
+#include <TartarusRPC.h>
+#include <TartarusRPCTypes.h>
+
+namespace Tartarus
+{
+
+class UserReaderPrx: public RPC::ObjectPrx
+{
+public:
+        UserReaderPrx(const std::string & name, RPC::Connection & connection):
+            ObjectPrx(name, connection) {}
+        UserRecord getUser(uid_t uid);
+        UserRecord getUser(const std::string &username);
+};
+
+class GroupReaderPrx: public RPC::ObjectPrx
+{
+public:
+        GroupReaderPrx(const std::string & name, RPC::Connection & connection):
+            ObjectPrx(name, connection) {}
+        GroupRecord getGroup(gid_t gid);
+        GroupRecord getGroup(const std::string &groupname);
+
+        std::vector<std::string> getUsers(gid_t gid);
+        std::vector<gid_t> getUserGroups(const std::string &username);
+};
+
+}
+
+#endif
